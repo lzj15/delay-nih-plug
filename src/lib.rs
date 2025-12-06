f567ff7211f4098e36a11c95181501367654e2a5
lzj15 <lzj15@proton.me>
2025-12-06 20:41:23 +0000

    Implement delay effect

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use nih_plug::prelude::*;
-use parking_lot::Mutex;
 use std::sync::Arc;
 
-struct Gain {
-    params: Arc<GainParams>,
+struct Delay {
+    params: Arc<DelayParams>,
+    deque: std::collections::VecDeque<f32>,
 }
 
 /// The [`Params`] derive macro gathers all of the information needed for the wrapper to know about
@@ -11,136 +11,66 @@ struct Gain {
 /// also easily implement [`Params`] by hand if you want to, for instance, have multiple instances
 /// of a parameters struct for multiple identical oscillators/filters/envelopes.
 #[derive(Params)]
-struct GainParams {
-    /// The parameter's ID is used to identify the parameter in the wrapped plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
-    #[id = "gain"]
-    pub gain: FloatParam,
+struct DelayParams {
+    #[id = "time"]
+    pub time: FloatParam,
 
-    /// This field isn't used in this example, but anything written to the vector would be restored
-    /// together with a preset/state file saved for this plugin. This can be useful for storing
-    /// things like sample data.
-    #[persist = "industry_secrets"]
-    pub random_data: Mutex<Vec<f32>>,
+    #[id = "feedback"]
+    pub feedback: FloatParam,
 
-    /// You can also nest parameter structs. These will appear as a separate nested group if your
-    /// DAW displays parameters in a tree structure.
-    #[nested(group = "Subparameters")]
-    pub sub_params: SubParams,
-
-    /// Nested parameters also support some advanced functionality for reusing the same parameter
-    /// struct multiple times.
-    #[nested(array, group = "Array Parameters")]
-    pub array_params: [ArrayParams; 3],
+    #[id = "mix"]
+    pub mix: FloatParam,
 }
 
-#[derive(Params)]
-struct SubParams {
-    #[id = "thing"]
-    pub nested_parameter: FloatParam,
-}
-
-#[derive(Params)]
-struct ArrayParams {
-    /// This parameter's ID will get a `_1`, `_2`, and a `_3` suffix because of how it's used in
-    /// `array_params` above.
-    #[id = "noope"]
-    pub nope: FloatParam,
-}
-
-impl Default for Gain {
+impl Default for Delay {
     fn default() -> Self {
         Self {
-            params: Arc::new(GainParams::default()),
+            params: Arc::new(DelayParams::default()),
+            deque: std::collections::VecDeque::new(),
         }
     }
 }
 
-impl Default for GainParams {
+impl Default for DelayParams {
     fn default() -> Self {
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
-            gain: FloatParam::new(
-                "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+            time: FloatParam::new(
+                "Time",
+                300.0,
+                FloatRange::Linear {
+                    min: 1.0,
+                    max: 2000.0,
                 },
             )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
-            .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
-            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-            // Persisted fields can be initialized like any other fields, and they'll keep their
-            // values when restoring the plugin's state.
-            random_data: Mutex::new(Vec::new()),
-            sub_params: SubParams {
-                nested_parameter: FloatParam::new(
-                    "Unused Nested Parameter",
-                    0.5,
-                    FloatRange::Skewed {
-                        min: 2.0,
-                        max: 2.4,
-                        factor: FloatRange::skew_factor(2.0),
-                    },
-                )
+            .with_value_to_string(formatters::v2s_f32_rounded(0))
+            .with_unit(" ms"),
+
+            feedback: FloatParam::new("Feedback", 0.5, FloatRange::Linear { min: 0.0, max: 1.0 })
                 .with_value_to_string(formatters::v2s_f32_rounded(2)),
-            },
-            array_params: [1, 2, 3].map(|index| ArrayParams {
-                nope: FloatParam::new(
-                    format!("Nope {index}"),
-                    0.5,
-                    FloatRange::Linear { min: 1.0, max: 2.0 },
-                ),
-            }),
+
+            mix: FloatParam::new("Mix", 0.5, FloatRange::Linear { min: 0.0, max: 1.0 })
+                .with_value_to_string(formatters::v2s_f32_rounded(2)),
         }
     }
 }
 
-impl Plugin for Gain {
-    const NAME: &'static str = "Gain";
-    const VENDOR: &'static str = "Moist Plugins GmbH";
+impl Plugin for Delay {
+    const NAME: &'static str = "Delay";
+    const VENDOR: &'static str = "lzj15";
     // You can use `env!("CARGO_PKG_HOMEPAGE")` to reference the homepage field from the
     // `Cargo.toml` file here
-    const URL: &'static str = "https://youtu.be/dQw4w9WgXcQ";
-    const EMAIL: &'static str = "info@example.com";
+    const URL: &'static str = "https://codeberg.org/lzj15";
+    const EMAIL: &'static str = "lzj15@proton.me";
 
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
     // The first audio IO layout is used as the default. The other layouts may be selected either
     // explicitly or automatically by the host or the user depending on the plugin API/backend.
-    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
-        AudioIOLayout {
-            main_input_channels: NonZeroU32::new(2),
-            main_output_channels: NonZeroU32::new(2),
-
-            aux_input_ports: &[],
-            aux_output_ports: &[],
-
-            // Individual ports and the layout as a whole can be named here. By default these names
-            // are generated as needed. This layout will be called 'Stereo', while the other one is
-            // given the name 'Mono' based no the number of input and output channels.
-            names: PortNames::const_default(),
-        },
-        AudioIOLayout {
-            main_input_channels: NonZeroU32::new(1),
-            main_output_channels: NonZeroU32::new(1),
-            ..AudioIOLayout::const_default()
-        },
-    ];
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+        ..AudioIOLayout::const_default()
+    }];
 
     const MIDI_INPUT: MidiConfig = MidiConfig::None;
     // Setting this to `true` will tell the wrapper to split the buffer up into smaller blocks
@@ -168,21 +98,42 @@ impl Plugin for Gain {
     // plugin. If we do need special initialization, we could implement the `initialize()` and/or
     // `reset()` methods
 
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>,
+    ) -> bool {
+        self.deque =
+            std::collections::VecDeque::with_capacity((2.0 * buffer_config.sample_rate) as usize);
+        true
+    }
+
     fn process(
         &mut self,
         buffer: &mut Buffer,
         _aux: &mut AuxiliaryBuffers,
-        _context: &mut impl ProcessContext<Self>,
+        context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        for channel_samples in buffer.iter_samples() {
-            // Smoothing is optionally built into the parameters themselves
-            let gain = self.params.gain.smoothed.next();
+        let time = self.params.time.value();
+        let feedback = self.params.feedback.value();
+        let mix = self.params.mix.value();
 
-            for sample in channel_samples {
-                *sample *= gain;
-            }
+        // Calculate the index of the sample before time interval specified
+        // -1.0 is there because the most recent sample in the queue has index 0
+        let index = (-1.0 + time * 0.001 * context.transport().sample_rate) as usize;
+
+        for mut channel_samples in buffer.iter_samples() {
+            let delay = *self.deque.get(index).unwrap_or(&0.0);
+            // Remove the last sample in the back of the queue to make room for next push_front()
+            self.deque
+                .remove((-1.0 + 2.0 * context.transport().sample_rate) as usize);
+            self.deque
+                .push_front(*channel_samples.get_mut(0).unwrap() + delay * feedback);
+            *channel_samples.get_mut(0).unwrap() =
+                *channel_samples.get_mut(0).unwrap() * (1.0 - mix) + delay * mix;
+            *channel_samples.get_mut(1).unwrap() = *channel_samples.get_mut(0).unwrap();
         }
-
         ProcessStatus::Normal
     }
 
@@ -191,24 +142,19 @@ impl Plugin for Gain {
     fn deactivate(&mut self) {}
 }
 
-impl ClapPlugin for Gain {
-    const CLAP_ID: &'static str = "com.moist-plugins-gmbh.gain";
-    const CLAP_DESCRIPTION: Option<&'static str> = Some("A smoothed gain parameter example plugin");
+impl ClapPlugin for Delay {
+    const CLAP_ID: &'static str = "lzj15.delay-nih-plug";
+    const CLAP_DESCRIPTION: Option<&'static str> =
+        Some("A very simple delay effect audio plugin using the nih-plug framework");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
-    const CLAP_FEATURES: &'static [ClapFeature] = &[
-        ClapFeature::AudioEffect,
-        ClapFeature::Stereo,
-        ClapFeature::Mono,
-        ClapFeature::Utility,
-    ];
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect];
 }
 
-impl Vst3Plugin for Gain {
-    const VST3_CLASS_ID: [u8; 16] = *b"GainMoistestPlug";
-    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
-        &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
+impl Vst3Plugin for Delay {
+    const VST3_CLASS_ID: [u8; 16] = *b"delay-nih-plug  ";
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] = &[Vst3SubCategory::Fx];
 }
 
-nih_export_clap!(Gain);
-nih_export_vst3!(Gain);
+nih_export_clap!(Delay);
+nih_export_vst3!(Delay);
